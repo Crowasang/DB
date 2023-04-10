bfa4cf3fb988fbe67a4163f99139cacedca40c19
Crowasang <115343930+Crowasang@users.noreply.github.com>
2023-04-10 12:20:50 +0900

    Add files via upload

--- 0404ch03ex.sql
+++ 0404ch03ex.sql
@@ -1,7 +1,7 @@
 /*박지성이 구매한 도서의 출판사 수*/
-SELECT count(publisher)
-FROM book, orders
-WHERE book.bookid=orders.bookid and custid=1;
+SELECT count(Distinct publisher)
+FROM book, orders, customer
+WHERE book.bookid=orders.bookid and customer.custid=orders.custid and customer.name like'박지성';
 
 /*박지성이 구매한 도서의 이름, 가격, 정가와 판매가격의 차이*/
 SELECT bookname, book.price, book.price-orders.saleprice
@@ -9,34 +9,46 @@ FROM book, orders
 WHERE orders.bookid = book.bookid and custid=1;
 
 /*박지성이 구매하지 않은 도서의 이름*/
-SELECT DISTINCT bookname
-FROM book, orders
-WHERE orders.bookid = book.bookid and NOT custid=1;
+SELECT bookname
+FROM book
+WHERE not exists(SELECT bookname
+                    from customer, orders
+                    WHERE customer.custid=orders.custid
+                            and orders.bookid=book.bookid
+                            and customer.name like '박지성');
 
 /*주문하지 않은 고객의 이름(부속질의 사용)*/
 SELECT name
 FROM customer
-WHERE NOT custid IN(SELECT custid
-                    FROM orders);
+WHERE name NOT IN(SELECT name
+                    FROM customer, orders
+                    WHERE customer.custid=orders.custid);
 
 /*주문 금액의 총액과 주문의 평균 금액*/
 SELECT sum(saleprice), avg(saleprice)
 FROM orders;
 
 /*고객의 이름과 고객별 구매액*/
-SELECT name, SUM(saleprice)
+SELECT name, SUM(saleprice) as total
 FROM customer, orders 
 WHERE customer.custid=orders.custid
-GROUP BY customer.name;
+GROUP BY name;
 
 /*고객의 이름과 고객의 구매한 도서 목록*/
-SELECT customer.name, book.bookname
+SELECT name, book.bookname
 FROM customer, orders, book
 WHERE orders.custid = customer.custid and orders.bookid=book.bookid;
 
 /*도서의 가격과 판매가격의 차이가 가장 많은 주문*/
-SELECT book.price, max(b1.saleprice)
+SELECT *
 FROM book, orders
-WHERE 
+WHERE book.bookid=orders.bookid and price-saleprice=(select max(price-saleprice)
+                                                        from book, orders
+                                                        WHERE book.bookid=orders.bookid);
 
 /*도서의 판매액 평균보다 자신의 구매액 평균이 더 높은 고객의 이름*/
+SELECT name, avg(saleprice)
+FROM customer, orders
+WHERE customer.custid=orders.custid
+GROUP BY name
+HAVING AVG(saleprice) > (SELECT AVG(saleprice) from orders);
